c3b9c42f5b1f887b0ee1d3e16d7968c9e5685a4a
charlietyn <charlietyn@gmail.com>
2024-03-14 12:33:40 -0400

    feature/error 7

--- resources/views/sale_planes.blade.php
+++ resources/views/sale_planes.blade.php
@@ -98,7 +98,7 @@
                 <div class="pricing-plan is-featured">
                     <div class="name"><?= $row->name ?></div>
                     <img src="<?= !empty($row->photo)?$row->photo:asset('assets/img/logos/logo/logo-platinum.svg') ?>" alt="">
-                    <div class="price money_to_fscx"><?= intval($row->price) ?></div>
+                    <div class="price money_to_fscx"><?=intval($row->price)/intval($row->month) ?></div>
                     <div class="trial"><?= $row->subtitle ?></div>
                     <hr>
                     <ul>
